a3020f5328e4c6df7f706b485500d464cc70b8aa
Yash Kaskar <yash.kaskar99@gmail.com>
2022-11-23 12:54:31 +0530

    rafc, rfc

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,33 @@
 
-function card(props) {
+// function card({text}) {
+//   return (
+//     <div>
+//       <h1> { text }</h1>
+//     </div>
+//   )
+// }
+
+// export default card
+
+
+
+// const card = ({text}) => {
+//   return (
+//     <div>
+//       <h6> { text }</h6>
+//     </div>
+//   )
+// }
+
+// export default card
+
+
+
+
+export default function card({text}) {
   return (
     <div>
-      <h1> { props.text }</h1>
+      <h2>{ text }</h2>
     </div>
   )
 }
-
-export default card
